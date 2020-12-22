097133cb145c748ab1ff4be1875654ae3b3899ad
Ashish Sahu <ashishsahu1999@hotmail.com>
2020-12-23 00:14:52 +0530

    image input and conversion complete

--- test.py
+++ test.py
@@ -8,14 +8,19 @@ import numpy as np
 # define a video capture object 
 vid = cv2.VideoCapture(0) 
 
+
+    
 while(True):
     ret, frame = vid.read() 
     #print(type(frame))
     print(np.array(frame).shape)
     frame1 = cv2.resize(frame,(30,30))
-    print(np.array(frame1).shape)
-    cv2.imshow('frame', frame) 
-    cv2.imshow('frame1', frame) 
+    image = np.expand_dims(frame1, axis=0)
+    image = np.array(image)
+    print(image)
+    
+
+    cv2.imshow('frame', frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
