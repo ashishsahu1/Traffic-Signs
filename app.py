a9dc1137887f751bd41306b6709795884fe0f1d6
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-01-04 23:32:03 +0530

    prediction successful
    acurrracy is not good

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
-# from keras.models import load_model
+from keras.models import load_model
 import os
-
+import cv2 
+import numpy as np
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/img/input/'
@@ -9,7 +10,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024
 app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 
-# model = load_model('Model/Outputs/finalModel.h5')
+model = load_model('Model/Outputs/finalModel.h5')
 
 
 @app.route('/')
@@ -20,8 +21,14 @@ def ault():
 def home():
     img = request.files['image']
     img.save('static/img/input/0.jpg')
-    
-    return render_template('pred.html')
+
+    img = cv2.imread('static/img/input/0.jpg')
+    img1 = cv2.resize(img,(30,30))
+    image = np.expand_dims(img1, axis=0)
+    img = np.array(image)
+    pred = model.predict_classes([image])[0]
+    clas = pred+1
+    return render_template('pred.html',clas=clas)
 
 
 if __name__ == "__main__":
