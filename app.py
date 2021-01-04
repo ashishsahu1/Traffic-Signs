1fa8c38e5720e316e1253805b1881b2152f81ca1
Ashish Sahu <ashishsahu1999@hotmail.com>
2021-01-04 21:46:20 +0530

    Routing Successful

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ import os
 
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'static/img/input/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_PATH'] = 16 * 1024 * 1024
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 1
 
 # model = load_model('Model/Outputs/finalModel.h5')
 
 
 @app.route('/')
-def hello_world():
+def ault():
     return render_template('index.html')
+
+@app.route('/home', methods = ['POST'])
+def home():
+    return render_template('pred.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
